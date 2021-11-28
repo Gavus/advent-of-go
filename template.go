fd1fc842657da416f55b5f60e32986f2e620bffc
Gustav Johansson <gustavsj@outlook.com>
2021-11-28 15:01:09 +0100

    Add template for days

---
+++ template.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Gavus/advent-of-code/utils/input"
+	_ "github.com/Gavus/advent-of-code/utils/log"
+)
+
+const (
+	question1 = "unknown"
+	question2 = "unknown"
+)
+
+func main() {
+	input := input.GetInput("2021-12-01", "")
+
+	solution1 := part1(input)
+	fmt.Println(question1, solution1)
+
+	solution2 := part2(input)
+	fmt.Println(question2, solution2)
+}
+
+func part1(input []string) int {
+	return 0
+}
+
+func part2(input []string) int {
+	return 0
+}
